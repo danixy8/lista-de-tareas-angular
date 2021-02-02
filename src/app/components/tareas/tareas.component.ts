6c5eacc33c3de35e4d68a689d1185b5419e04360
Daniel Gutierrez <danielguve2@gmail.com>
2021-02-02 15:53:34 -0300

    Commit de lista

---
+++ src/app/components/tareas/tareas.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Tarea } from 'src/app/models/Tarea';
+
+@Component({
+  selector: 'app-tareas',
+  templateUrl: './tareas.component.html',
+  styleUrls: ['./tareas.component.css']
+})
+export class TareasComponent implements OnInit {
+  listTareas: Tarea[] = [];
+  nombreTarea = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  agregarTareas(): void {
+
+    //crear un objeto tarea
+    const tarea: Tarea = {
+      nombre: this.nombreTarea,
+      estado: false
+    };
+
+    //agregar el objeto al array
+    this.listTareas.push(tarea);
+
+    //resetear form
+    this.nombreTarea = '';
+    }
+
+    eliminarTarea(index: number): void{
+      this.listTareas.splice(index, 1);
+  }
+
+  actualizarTarea(tarea: Tarea, index: number): void {
+    this.listTareas[index].estado = !tarea.estado;
+    console.log(this.listTareas);
+  }
+
+}
